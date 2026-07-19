e044e10e02db9ca08c5eec27e0d12661e35c75c7
Jinay Shah <jinay@Jinays-MacBook-Pro.local>
2026-07-19 19:13:36 +0530

    vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // Read environment variables from the project root
   envDir: '../',
+
+  // Build output inside frontend/dist
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
